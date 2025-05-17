8630dfbf9603e5a39bcc9f7748da40d42fb9e9ee
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-05-18 05:22:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", None))
-MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "FRIENDSHIP_CHAT_GROUP0")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "LOVE_ME_BABBY")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "@virat_0011")
+API_ID = int(getenv("API_ID", "24313439"))
+API_HASH = getenv("API_HASH", "a007ea183e9276803caba61ad093273b")
+BOT_TOKEN = getenv("BOT_TOKEN", "7718680866:AAEjIwxBUaNBE9lBwFNxq3X8YaTGZdqMqWM")
+OWNER_ID = int(getenv("OWNER_ID", "7290350162"))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://botbaby:botbaby@cluster0.zngip1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "https://t.me/infinitygx_bot_support")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "https://t.me/gxinfinity_support")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "@Its_tg_seller")
 
 # Random Start Images
 IMG = [
